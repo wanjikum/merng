2edef9618b13dcfe1dc7d947de9ab2a70bc15c89
MillicentNj <millicent.njuguna@allangray.co.za>
2021-05-25 23:25:52 +0300

    Add register user validators

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,9 +1,14 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import apolloServer from 'apollo-server';
 
 
 import User from '../../models/User.js';
 import config from '../../config.js';
+import validators from './utils/user-validator.js'
+
+
+const { UserInputError } = apolloServer;
 
 const { SALT_ROUNDS, SECRET_KEY } = config;
 
@@ -11,9 +16,27 @@ const resolvers = {
     Mutation: {
         async register(_, { registerInput }) {
             try {
-                const { password, username, confirmPassword, email } = registerInput;
+                const { password, username, email } = registerInput;
+                const { errors, valid } = validators.validateRegisterInput(registerInput);
+                console.log(">>>errors", errors, valid)
+
+                // Validate user data
+                if (!valid) {
+                    throw new UserInputError('Errors', { errors });
+                  }
+                
+                // Check if user exists
+                const existingUser = await User.findOne({ username });
+
+                if(existingUser) {
+                    throw new UserInputError('Username is already taken!', {
+                        errors: {
+                            username: "This username is already taken"
+                        }
+                    })
+                }
                 const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
-                console.log(">>>>>", password, hashedPassword);
+
                 const newUser =  new User({
                     username,
                     password: hashedPassword,
@@ -21,7 +44,6 @@ const resolvers = {
                     createdAt: new Date().toISOString()
                 })
                 const res = await newUser.save();
-                console.log("res>>>", res._doc);
 
                 const token = jwt.sign({
                     id: res._id,
